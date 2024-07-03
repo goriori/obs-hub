d3353e4d38921ff366f07b0c4adf2c6322b2168d
goriori <vladislav.borisov2010@gmail.com>
2024-07-03 10:52:09 +0400

    change: - change buildPositionApplication.js for computed position y

--- src/utils/helpers/buildPositionApplication.js
+++ src/utils/helpers/buildPositionApplication.js
@@ -1,7 +1,7 @@
 export const buildPositionApplication = (position, resolutionApplication, mainScreenPosition) => {
     return {
         x: position.x * (mainScreenPosition.width / Number(resolutionApplication[0])) || 0,
-        y: (mainScreenPosition.height / Number(resolutionApplication[1])) || 0,
+        y: position.y * (mainScreenPosition.height / Number(resolutionApplication[1])) || 0,
         width: position.width * (mainScreenPosition.width / Number(resolutionApplication[0])) || 0,
         height: position.height * (mainScreenPosition.height / Number(resolutionApplication[1])) || 0
     }
